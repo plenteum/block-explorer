64215e4da7df2451ed3d0ffe2c245b125116630a
Dave <dave@thisisembrace.com>
2018-09-14 13:03:42 +0200

    Added clevery pool

--- config.js
+++ config.js
@@ -10,13 +10,14 @@ var refreshDelay = 30000;
 //["know.miningpool.gq/plenteum/", "http://ple.miningpool.gq:8197"],
 var networkStat = {
  "ple": [
-	["plenteum.miner.care", "http://85.216.148.43:8110"],
-    ["crypto9coin.cf/ple", "https://crypto9coin.cf:44019"],
+	 ["plenteum.miner.care", "http://85.216.148.43:8110"],
+     ["crypto9coin.cf/ple", "https://crypto9coin.cf:44019"],
      ["getpool.org/ple", "https://api.getpool.org/rpc/ple/"],
      ["ple.optimusblue.com", "http://ple.optimusblue.com:8117"],
      ["plenteum.virtopia.ca", "https://ple.virtopia.ca"],
 	 ["45.199.183.128","http://45.199.183.128:8117"],
-	 ["ple.cnpool.vip", "https://www.cnpool.vip:9249"]
+     ["ple.cnpool.vip", "https://www.cnpool.vip:9249"],
+     ["plenteum.clevery.xyz", "https://plenteum.clevery.xyz/api/"]
  ]
 };
 
