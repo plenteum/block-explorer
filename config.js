943072c5c747a2de786978ce4c6e6fbaca71a11e
Dave <david@itbsoftware.co.za>
2019-01-25 09:10:20 +0200

    new pool

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-var api = 'http://142.93.132.203:44016';
+var api = 'http://206.189.124.145:44016';
 var donationAddress = "";
 var blockTargetInterval = 120;
 var coinUnits = 100000000; //atomic units after the decimal place
@@ -10,17 +10,16 @@ var refreshDelay = 30000;
 //["know.miningpool.gq/plenteum/", "http://ple.miningpool.gq:8197"],
 var networkStat = {
  "ple": [
-	 ["plenteum.miner.care", "http://85.216.148.43:8110"],
+	 ["plenteum.miner.care", "https://ple.miner.care"],
      ["crypto9coin.cf/ple", "https://crypto9coin.cf:44019"],
-     ["getpool.org/ple", "https://api.getpool.org/rpc/ple/"],
      ["ple.optimusblue.com", "http://ple.optimusblue.com:8117"],
      ["plenteum.virtopia.ca", "https://ple.virtopia.ca"],
-     ["ple.cnpool.vip", "https://www.cnpool.vip:9249"],
      ["plenteum.clevery.xyz", "https://plenteum.clevery.xyz/api/"],
-	 ["plenteumpool.xyz", "https://plenteumpool.xyz/api"],
      ["ple.llama.horse", "http://pool.llama.horse:44018"],
-     ["the-miners.de/plenteum", "http://173.212.249.18:8112"],
-     ["ple.cnhub.de", "http://ple.cnhub.de:18117"]
+     ["the-miners.de/Plenteum", "http://173.212.249.18:8115"],
+	 ["youpool.io/PLE", "http://144.202.120.143:8111"],
+     ["plenteum.pool.private-locker.com", "https://plenteum.pool.private-locker.com/api/"],
+     ["plenteum.smartcoinpool.com", "https://plenteum.smartcoinpool.com:5140"]
  ]
 };
 
